32bff499bf51a14e178e594d2efd23ab95354388
dolmo17 <daolmo@uchicago.edu>
2025-01-22 16:00:09 -0600

    exercise complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(first, second) {
+    return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+    return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+    return nums.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+    let prod = 1;
 
+    for (let i = 0; i < nums.length; i++) {
+        prod *= nums[i];
+    }
+
+    return nums.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+    let prod = num;
+
+    if (pow == 0) {
+        return 1;
+    }
+
+    for (let i = 1; i < pow; i++) {
+        prod = multiply([prod, num]);
+    }
+
+    return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+    if (val == 0) {
+        return 1;
+    }
+
+    return factorial(val - 1) * val;
 };
 
 // Do not edit below this line
